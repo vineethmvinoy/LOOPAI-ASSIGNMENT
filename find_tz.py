dc12bb484d22b6fd6ec1a6ccbd8dc628ecd946fc
Vineeth M Vinoy <vineethmvinoy@gmail.com>
2023-11-14 02:55:23 +0530

    File To Return the Local Timezone

--- find_tz.py
+++ find_tz.py
@@ -6,9 +6,11 @@ def find_tz(curr,row):
   #Query to find the timezone of a store using its store id from store_timezone table
 
   if(curr.rowcount==0):
-        store_timezone='America/Chicago'         #if no timezones are found from the above query the timezone is set as 'America/Chicago'
+        store_timezone='America/Chicago'         
         local_tz=pytz.timezone(store_timezone) 
+        #IF no timezones are found from the above query the timezone is set as 'America/Chicago'
   else:
         store_timezone=curr.fetchone()
-        local_tz=pytz.timezone(store_timezone[1]) #Assigning Timezones
+        local_tz=pytz.timezone(store_timezone[1])
+         #Assigning Timezones
   return local_tz
