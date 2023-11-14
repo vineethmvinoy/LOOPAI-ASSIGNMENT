be4be0fe6ca5114004288fd7bcfd9afc7c5b8ef3
Vineeth M Vinoy <vineethmvinoy@gmail.com>
2023-11-14 13:02:56 +0530

    Function to return report data from the table it is not returning anything

--- displayResult.py
+++ displayResult.py
@@ -3,28 +3,41 @@ from dbConnect import dbConnect
 def displayResult(report_id):
   try:
     finaldata=[]
+
     #temporary placeholder for finaldata to be returned
     
     conn=dbConnect()
+
     #acquiring db connection
     
     curr=conn.cursor()
+
     #assigning cursor from db connection
+
     curr.execute('select * from public."REPORT_MAPPING" where report_id=%s',(report_id,))
+
     #query to check whether the process has been completed or not using the status attribute
+
     if(curr.rowcount==0):
       finaldata={'status':'Invalid Report Id'}
+
     #Condition to check whether the given report id in the url is correct or not
+
     else:
       Report_Status=curr.fetchone()
       if(Report_Status[1].strip()=='RUNNING'):
         finaldata={'report_Id':report_id,'status':'RUNNING'}
+
       #if report id is found and status is running return running status
+
       else:
         curr.execute('select * from public."REPORTS" where report_id=%s',(report_id,))
         REPORTS=curr.fetchall()
+
         #if returned status as complete assign all the generated values onto a dictionary and return
+        
         for row in REPORTS:
+
           finaldata.append(
                           {
                             'store_id':row[1],
@@ -36,10 +49,13 @@ def displayResult(report_id):
                             'downtime_last_week':row[7]
                           }
                           )                
-        finaldata={'report_id':report_id,
+        finaldata={'REPORTS':finaldata,
+                   'report_id':report_id,
                 'status':'COMPLETED',
-                'REPORTS':finaldata}
+                }
+        
     #assigning the placeholder with required values
+    
     return finaldata
     #return the placeholder
   except Exception as error:
