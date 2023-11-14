d275235d2441c6ef24fea6d5960efce32da620e8
Vineeth M Vinoy <vineethmvinoy@gmail.com>
2023-11-14 13:09:45 +0530

    Function that generates the report id and starts the thread

--- trigger_report.py
+++ trigger_report.py
@@ -5,16 +5,23 @@ import random
 import string
 
 def generate_report(count):
+  
   report_id=''.join(random.choices(string.ascii_uppercase +
                              string.digits, k=7))
   report_id=str(report_id)
+  #Random report ID generator to access the created reports later
+  
   thread=threading.Thread(target=trigger_report,args=(report_id,count,))
   thread.start()
+  #threading the logic program to display status running
+  
   try:
     conn=dbConnect()
     curr=conn.cursor()
     curr.execute('INSERT INTO public."REPORT_MAPPING" VALUES(%s,%s)',(report_id,'RUNNING'))
     conn.commit()
+    
+    #inserting the new report id into the table and setting status as running until completed
     
   except Exception as error:
     print(error)
