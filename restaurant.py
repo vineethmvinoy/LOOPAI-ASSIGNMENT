774a0a8491ecb03f8b03eadacca88bb9e7b7422a
Vineeth M Vinoy <vineethmvinoy@gmail.com>
2023-11-14 13:12:14 +0530

    run.py with routes

--- restaurant.py
+++ restaurant.py
@@ -1,14 +1,14 @@
 from trigger_report import generate_report
 from displayResult import displayResult
-from flask import Flask
+from flask import Flask,jsonify
 app=Flask(__name__)
 @app.route('/get_report/<string:report_id>',methods=['GET'])
 def get_report(report_id):
  finaldata=displayResult(report_id)
- return finaldata
+ return jsonify(finaldata)
 
-@app.route('/trigger_report',methods=['GET'])
-def homepage():
-  return generate_report(30)
+@app.route('/trigger_report',methods=['GET'])    #trigger_report 
+def trigger_report():
+  return generate_report(4)
 if __name__=="__main__":
   app.run(debug=True)
